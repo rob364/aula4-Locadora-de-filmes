3fc51a6d1f3b2603b0a7776290202fadd6e63515
Roberta <vcindustria@gmail.com>
2022-05-19 22:30:02 -0300

    Primeiro comite

---
+++ conteudo/ex.js
@@ -0,0 +1,23 @@
+var listaFilmes = 
+["https://images-na.ssl-images-amazon.com/images/I/91jr985qa7L.jpg", 
+"https://upload.wikimedia.org/wikipedia/pt/d/de/White_chicks.jpeg",
+"https://upload.wikimedia.org/wikipedia/pt/2/22/Titanic_poster.jpg",
+"https://br.web.img2.acsta.net/medias/nmedia/18/97/26/66/20519352.jpg",
+"https://br.web.img2.acsta.net/medias/nmedia/18/86/96/34/20028591.jpg", 
+"https://br.web.img3.acsta.net/pictures/18/05/14/17/09/5117345.jpg", 
+"https://br.web.img2.acsta.net/medias/nmedia/18/91/08/82/20128877.JPG", 
+"https://upload.wikimedia.org/wikipedia/pt/7/7d/Aquaman_%282018%29.jpg", 
+"https://br.web.img2.acsta.net/pictures/17/10/23/19/55/0260439.jpg", 
+"https://br.web.img3.acsta.net/pictures/17/10/11/17/34/4815573.jpg", 
+"https://p2.trrsf.com/image/fget/cf/648/0/images.terra.com/2021/10/08/1472715160-wofts1onesheetheroesvillainspre27x40thafinalpt-br.jpg",
+"https://1.bp.blogspot.com/-rCpjdvVJlMk/X86SDCT4S8I/AAAAAAAAGOs/6a8zIDyK0dMPTU_b-a9bHaiCr54SrDkQwCLcBGAsYHQ/s430/peaky-blinders.png",
+"https://br.web.img3.acsta.net/pictures/20/11/04/11/32/4052345.jpg",
+"https://blogdovalente.com.br/wp-content/uploads/2019/11/00.jpg"];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+}
+
+
+
+
